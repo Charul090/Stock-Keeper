0611395f662689e1c9606c47170ff5b51e763d62
Charul Bari <baricharul9@hotmail.com>
2020-05-23 13:47:20 +0530

    feat:Backend add item quantity

--- backend/server.py
+++ backend/server.py
@@ -39,3 +39,69 @@ def sendInfo():
         "current_page":page,
         "data":data
     })
+
+
+@app.route("/stock/add/<user_id>",methods=["GET","POST"])
+def addItem(user_id):
+    if request.method == "GET":
+        data=""
+
+        with open("data/info.csv","r") as file_handler:
+            file_content=csv.DictReader(file_handler)
+
+            for x in file_content:
+                if x["stock_id"] == user_id:
+                    data=x
+                    break
+                    
+        return json.dumps({
+            "data":data
+        })
+    else:
+        if request.method == "POST":
+            add=request.json["add"]
+            time=request.json["time"]
+
+            list1 = []
+            diff = {}
+
+            with open("data/info.csv","r") as file_handler:
+                file_content=csv.DictReader(file_handler)
+
+                for x in file_content:
+                    list1.append(x)
+
+            data=[]
+
+            for x in range(len(list1)):
+
+                if list1[x]["stock_id"] == user_id:
+                    quantity = int(list1[x]["quantity"])+int(add)
+
+                    dict1={
+                        "stock_id":list1[x]["stock_id"],
+                        "quantity":quantity,
+                        "item":list1[x]["item"],
+                        "price":list1[x]["price"]
+                    }
+                    
+                    data.append(dict1)
+                else:
+                    data.append(list1[x])
+
+            headers=data[0].keys()
+
+            
+
+            with open("data/info.csv","w") as file_handler:
+                csv_write=csv.DictWriter(file_handler,fieldnames=headers)
+
+                csv_write.writeheader()
+                csv_write.writerows(data)
+
+
+            return json.dumps({
+                "message":"Item added Successfully"
+            })
+
+
